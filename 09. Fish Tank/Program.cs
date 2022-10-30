7ea0accb2d03045e7e1e85a00ad19609bf357d37
nanji <nanji@DESKTOP-4A8MIDK>
2022-10-30 13:28:11 +0200

    Add project files.

---
+++ 09. Fish Tank/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _09._Fish_Tank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         int length = int.Parse(Console.ReadLine());
+         int width = int.Parse(Console.ReadLine());
+         int height = int.Parse(Console.ReadLine());
+         double perecentage = double.Parse(Console.ReadLine());
+            // calculate
+            int volume = length * width * height;
+            double maxLitres = volume / 1000.0;
+            double percentagedividedByHundred = perecentage / 100;
+            double neededLitres = maxLitres - (maxLitres * percentagedividedByHundred);
+            // output
+            Console.WriteLine(neededLitres);
+            
+           
+        }
+    }
+}
